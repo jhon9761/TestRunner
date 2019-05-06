bbd760a7593b87a131eecec60affceb5b41cd564
Bickio O'Callahan <bickioocallahan@gmail.com>
2019-05-06 12:18:10 +1200

    Add timing

--- test-runner.py
+++ test-runner.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from difflib import Differ
 from textwrap import indent
 import shlex
+import time
 
 DEFAULT_TIMEOUT = 2
 DEFAULT_TESTS_DIR = './tests'
@@ -10,66 +11,74 @@ DEFAULT_TESTS_DIR = './tests'
 class TestRunner:
     def __init__(self, command, tests=None, timeout=DEFAULT_TIMEOUT):
         if tests == None:
-            testsPath = Path(DEFAULT_TESTS_DIR)
-            if not testsPath.is_dir():
-                testsPath = Path('.')
+            tests_path = Path(DEFAULT_TESTS_DIR)
+            if not tests_path.is_dir():
+                tests_path = Path('.')
         else:
-            testsPath = Path(tests)
-            if not testsPath.is_dir() :
+            tests_path = Path(tests)
+            if not tests_path.is_dir() :
                 raise ValueError("tests must be an existing folder path string")
-        self.testsPath = testsPath
+        self.tests_path = tests_path
         self.command = shlex.split(command)
         self.timeout = timeout
 
-    def _test(self, inputFile, outputFile):
-        inputFile = inputFile.open()
-        outputFile = outputFile.open()
+    def _test(self, input_file, output_file):
+        input_file = input_file.open()
+        output_file = output_file.open()
 
-        execution = Popen(self.command, stdin=inputFile, stdout=PIPE, stderr=PIPE,
+        start_time = time.time()
+
+        execution = Popen(self.command, stdin=input_file, stdout=PIPE, stderr=PIPE,
                           universal_newlines=True)
         try:
             output, errors = execution.communicate(timeout=self.timeout)
         except TimeoutExpired:
             execution.kill()
-            print(f'Input file "{inputFile.name}" timed out\n')
+            print(f'Input file "{input_file.name}" timed out\n')
             return False
 
+        run_time = time.time() - start_time
+
         output = output.splitlines(keepends=True)
-        correctOutput = outputFile.readlines()
+        correct_output = output_file.readlines()
 
         if errors:
-            print(f'Input file "{inputFile.name}" caused one or more errors:\n')
+            print(f'Input file "{input_file.name}" caused one or more errors:\n')
             print(indent(errors, '    '))
             return False
 
-        if output != correctOutput:
-            print(f'Input file "{inputFile.name}" gave incorrect output:\n')
+        if output != correct_output:
+            print(f'Input file "{input_file.name}" gave incorrect output:\n')
             differ = Differ()
-            for line in differ.compare(correctOutput, output):
+            for line in differ.compare(correct_output, output):
                 print(line, end='')
             print()
             return False
 
-        return True
+        print(f'Input file "{input_file.name}" finished in {round(run_time, 3)} seconds')
+        return run_time
 
     def run_test(self, testName):
-        inputFile = next(self.testsPath.glob(f'{testName}-input.txt')).open()
-        outputFile = next(self.testsPath.glob(f'{testName}-output.txt')).open()
+        input_file = next(self.tests_path.glob(f'{testName}-input.txt')).open()
+        output_file = next(self.tests_path.glob(f'{testName}-output.txt')).open()
 
-        self._test(inputFile, outputFile)
+        self._test(input_file, output_file)
 
     def run_all_tests(self):
-        inputFiles = sorted(self.testsPath.glob('*-input.txt'))
-        outputFiles = sorted(self.testsPath.glob('*-output.txt'))
-        if len(inputFiles) != len(outputFiles):
+        input_files = sorted(self.tests_path.glob('*-input.txt'))
+        output_files = sorted(self.tests_path.glob('*-output.txt'))
+        if len(input_files) != len(output_files):
             raise ValueError('different number if input files to output files')
 
-        total = len(inputFiles)
+        total = len(input_files)
         passed = 0
-        for inputFile, outputFile in zip(inputFiles, outputFiles):
-            if self._test(inputFile, outputFile):
+        total_time = 0
+        for input_file, output_file in zip(input_files, output_files):
+            output = self._test(input_file, output_file)
+            if output:
                 passed += 1
-        print(f"{passed}/{total} tests passed")
+                total_time += output
+        print(f"{passed}/{total} tests passed in {round(total_time, 1)} seconds")
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
